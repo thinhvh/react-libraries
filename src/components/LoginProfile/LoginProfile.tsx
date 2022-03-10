7dd71e8a42f3a87ab1722946bef1205a5da9b844
Thinh Vo <thinhv@intelisysaviation.com>
2022-03-10 08:59:33 -0500

    update basic animation for toggle menu

--- src/components/LoginProfile/LoginProfile.tsx
+++ src/components/LoginProfile/LoginProfile.tsx
@@ -1,22 +1,29 @@
-import { SyntheticEvent, useState } from 'react';
-import Portal from '../Portal/Portal';
-import styles from './LoginProfile.module.scss';
+import { SyntheticEvent, useState } from "react";
+import Portal from "../Portal/Portal";
+import styles from "./LoginProfile.module.scss";
 
 export interface LoginProfileProps {
   fullName: string;
   size: string;
+  onLogout?: Function;
 }
 
 const LoginProfile: React.FC<Partial<LoginProfileProps>> = (props) => {
-  const { fullName = '', size = '32px' } = props;
+  const { fullName = "", size = "32px", onLogout } = props;
   const [openProfileMenu, setOpenProfileMenu] = useState(false);
   const [anchor, setAnchor] = useState<EventTarget & HTMLButtonElement>();
   const firstCharacter = fullName.charAt(0);
 
-  const onClickProfile = (event: SyntheticEvent<HTMLButtonElement, MouseEvent>) => {
+  const onClickProfile = (
+    event: SyntheticEvent<HTMLButtonElement, MouseEvent>
+  ) => {
     setOpenProfileMenu(!openProfileMenu);
     setAnchor(event.currentTarget);
-  }
+  };
+
+  const logout = () => {
+    onLogout?.();
+  };
 
   return (
     <>
@@ -24,16 +31,14 @@ const LoginProfile: React.FC<Partial<LoginProfileProps>> = (props) => {
         className={styles.loginContainer}
         style={{
           width: size,
-          height: size
+          height: size,
         }}
       >
-        <button onClick={onClickProfile}>
-          {firstCharacter}
-        </button>
+        <button onClick={onClickProfile}>{firstCharacter}</button>
       </div>
       <Portal open={openProfileMenu} anchor={anchor}>
         <div className={styles.menu}>
-          <div className={styles.logout} role="menuitem">
+          <div className={styles.logout} role="menuitem" onClick={logout}>
             Log out
           </div>
         </div>
